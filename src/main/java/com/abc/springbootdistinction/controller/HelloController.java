1c79b426887447eef1681234141e406a0ba39d82
masahito <imiplayacc@gmail.com>
2022-09-24 05:33:22 +0900

    Checked Get and Post.

--- src/main/java/com/abc/springbootdistinction/controller/HelloController.java
+++ src/main/java/com/abc/springbootdistinction/controller/HelloController.java
@@ -1,13 +1,33 @@
 package com.abc.springbootdistinction.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false) String message, Model model) {
+        model.addAttribute("sample", message);
+        return "hello";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+
+    @PostMapping("/confirm")
+    public String confirm(@RequestParam String message, Model model) {
+        model.addAttribute("message", message);
+        return "confirm";
     }
 }
