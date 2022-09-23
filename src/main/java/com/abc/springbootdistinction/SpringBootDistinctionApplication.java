c1dccac26460e5c05d6d55fe5c52fa28c1d45d65
masahito <imiplayacc@gmail.com>
2022-09-24 04:48:38 +0900

    initial commit.

---
+++ src/main/java/com/abc/springbootdistinction/SpringBootDistinctionApplication.java
@@ -0,0 +1,13 @@
+package com.abc.springbootdistinction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDistinctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDistinctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/springbootdistinction/SpringBootDistinctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.springbootdistinction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDistinctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
